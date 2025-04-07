60fb9f0d96e1b313cadce8fd5e3b8aeccd4cec70
arjunthazath <arjunta32@gmail.com>
2025-04-07 20:10:03 +0530

    ws room logic

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,7 +1,33 @@
-import { WebSocketServer } from "ws";
+import { WebSocket, WebSocketServer } from "ws";
 import jwt from 'jsonwebtoken'
 import { JWT_SECRET } from '@repo/backend-common/config';
 const wss= new WebSocketServer({port:8080})
+
+
+interface User{
+    ws:WebSocket,
+    rooms: string[],
+    userId:string
+}
+
+
+const users: User[] = [] 
+
+function checkUser(token: string): string | null{
+    const decoded= jwt.verify(token,JWT_SECRET as string)
+
+    if(typeof token=="string"){
+        return null
+    }
+
+    if(!decoded){
+        return null
+    }
+    //@ts-ignore
+    return decoded.userId
+}
+
+
 
 wss.on("connection", function connection(ws,req){
 
@@ -13,13 +39,19 @@ wss.on("connection", function connection(ws,req){
     const queryParams= new URLSearchParams(url.split("?")[1])
 
     const token= queryParams.get("token") || "";
-
-    const decoded= jwt.verify(token,JWT_SECRET as string)
-
-    if(!decoded){
+    const userId= checkUser(token)
+    
+    if(userId==null){
         ws.close()
         return
     }
+
+    users.push({
+        userId,
+        rooms:[],
+        ws
+
+    })
 
     ws.on("message", function message(data){
         ws.send(`The message sent was ${data}`)
